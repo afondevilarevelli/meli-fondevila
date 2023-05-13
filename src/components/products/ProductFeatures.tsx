3a852491350adb1c498f2bce677b82fb34443bdc
afondevilarevelli <antonny.42@gmail.com>
2023-05-13 16:30:41 -0300

    Product features component

--- src/components/products/ProductFeatures.tsx
+++ src/components/products/ProductFeatures.tsx
@@ -1,8 +1,26 @@
 import { useProductContext } from "@/contexts/ProductContext";
+import { Text, Icon, SimpleGrid, Stack } from "@chakra-ui/react";
 import React from "react";
+import { FaExclamationCircle } from "react-icons/fa";
 
 export default function ProductFeatures() {
   const { product } = useProductContext();
 
-  return <div>ProductFeatures</div>;
+  return (
+    <SimpleGrid columns={2} spacing={6}>
+      {product?.features.map((feature, idx) => (
+        <Stack key={idx} direction={"row"} spacing={5} align={"flex-start"}>
+          <Icon
+            as={FaExclamationCircle}
+            color={"gray.500"}
+            boxSize={6}
+            mt={0.5}
+          />
+          <Text>
+            <b>{feature.label}</b>: {feature.value}
+          </Text>
+        </Stack>
+      ))}
+    </SimpleGrid>
+  );
 }
